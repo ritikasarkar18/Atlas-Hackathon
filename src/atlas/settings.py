b630144eee3f2d89c6be61c76203f170e645ac65
ritikasarkar18 <ritikasarkar438@gmail.com>
2020-06-27 22:22:22 +0530

    More changes

--- src/atlas/settings.py
+++ src/atlas/settings.py
@@ -121,4 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = 'staticfiles'
+
+# this is directory paths where you have to put your project level static files
+# you can put multiple folders here
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
