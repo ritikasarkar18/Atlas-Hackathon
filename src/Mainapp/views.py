929dce352a184b5bdc315f1b07528a1357cb7b3f
Pratik Garai <pratikgarai0208@gmail.com>
2020-06-28 13:11:00 +0530

    Django form discarded, html form processed

--- src/Mainapp/views.py
+++ src/Mainapp/views.py
@@ -1,26 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import inputform as inputform1
+# from .forms import inputform as inputform1
 
 # Create your views here.
 def home(request):
-	return render(request,"index.html",{})
+    return render(request,"index.html",{})
 def forminput(request):
-	if request.method=='POST':
-                print(request.POST)
-		inputform = inputform1(request.POST)
-		if inputform.is_valid():
-                        print('Hello')
-                        print(inputform.cleaned_data)
-			ML(inputform.cleaned_data)
-		else:
-			#render error  page here
-                        print('Error')
-			return render(request,"error.html",{})
-	
-	#render the index page here
-	return render(request,"index.html",{})
+    if request.method=='POST':
+        dic = dict(request.POST)
+        fl = request.FILES 
+        ML(int(dic['style'][0]), dic['color'][0], float(dic['slider'][0]),fl['pdf']) 
+    return render(request,"index.html",{})
 
-def ML(data):
-	#handle it from here
-	pass
+def ML(style, color, thickness, fl):
+    print(style)
+    print(color)
+    print(thickness)
+    print(fl)
