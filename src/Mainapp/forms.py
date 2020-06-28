bcdfc1289764d974b58bd5daa70d357ee6c1bb84
ritikasarkar18 <ritikasarkar438@gmail.com>
2020-06-28 11:41:27 +0530

    error page and download button

--- src/Mainapp/forms.py
+++ src/Mainapp/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
-styles=(("Choose style","Choose style"),("Style 1","Style 1"),("Style 2","Style 2"),("Style 3","Style 3"),("Style 4","Style 4"),
+styles=(("Style 1","Style 1"),("Style 2","Style 2"),("Style 3","Style 3"),("Style 4","Style 4"),
 	("Style 5","Style 5"),("Style 6","Style 6"),("Style 7","Style 7"),("Style 8","Style 8"),("Style 9","Style 9"),
 	("Style 10","Style 10"))
-colors=(("Choose color","Choose color"),("Black","Black"),("Blue","Blue"),("Red","Red"),("Green","Green"))
+colors=(("Black","Black"),("Blue","Blue"),("Red","Red"),("Green","Green"))
 
 class inputform(forms.Form):
 	pdf = forms.FileField()
